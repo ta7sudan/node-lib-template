2debd4bd2d3b349851d5c16c2bbed578d251634e
ta7sudan <ta7sudan@gmail.com>
2018-11-11 21:56:52 +0800

    bug fixed

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,12 @@
+'use strict';
 const test = require('ava');{{if hasPock}}
 require('./_pock');{{/if}}
 
 /* global todo */
 
 
-const sleep = time => new Promise(rs => setTimeout(rs, time));
-const isObj = o => Object.prototype.toString.call(o) === '[object Object]';
+// const sleep = time => new Promise(rs => setTimeout(rs, time));
+// const isObj = o => Object.prototype.toString.call(o) === '[object Object]';
 
 test('todo', async t => {
 	t.pass();
